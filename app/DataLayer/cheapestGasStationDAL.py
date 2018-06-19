5e2f670d139eabd8b7afcc3821c8bb20a2e15aba
Alexis CHOLLET-EXT <alexis.chollet-ext@fnacdarty.com>
2018-06-19 14:36:29 +0200

    Creation of the DAL

--- app/DataLayer/cheapestGasStationDAL.py
+++ app/DataLayer/cheapestGasStationDAL.py
@@ -1,1 +1,24 @@
 import models
+from app import db
+
+def GetAllStationGas():
+    return pass
+
+def GetCarburants():
+    return pass
+
+def GetSationGasById(id):
+    return pass
+
+def GetAllLatestStationGasData():
+    return pass
+
+def GetStationGasByCarburantId(carburantId):
+    return pass
+
+def AddStationGasData():
+    return pass
+
+def AddStationGas():
+    return pass
+
